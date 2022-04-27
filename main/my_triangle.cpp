37a1f4579172906edbb2107a84590a9637f4a6aa
SandyWoolman <71042617+SandyWoolman@users.noreply.github.com>
2022-04-27 11:41:51 +0200

    created my triangle exe

---
+++ main/my_triangle.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <GL/glew.h>
+
+#include "engine.h"
+#include "file_utility.h"
+#include "scene.h"
+
+namespace gpr5300
+{
+    
+}
+int main(int argc, char** argv)
+{
+   
+}
