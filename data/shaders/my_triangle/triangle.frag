9c3aa871c6f49e4d95513c776d0765b6d32bb349
SandyWoolman <71042617+SandyWoolman@users.noreply.github.com>
2022-04-27 15:32:22 +0200

    changed and works

--- data/shaders/my_triangle/triangle.frag
+++ data/shaders/my_triangle/triangle.frag
@@ -1,20 +1,12 @@
-/*#version 310 es
-precision highp float;
+#version 300 es
+precision mediump float;
 
-in vec3 fragColor;
+layout(location = 0) out vec4 fragColor;
+in vec3 outColor;
 
-layout(location = 0) out vec4 outColor;
+uniform float colorCoeff;
 
 void main(){
-	outColor = vec4(fragColor, 1.0f);
+	fragColor = vec4(outColor * colorCoeff, 1.0);
 }
-*/
 
-#version 330 core
-
-out vec4 FragColor;
-
-void main()
-{
-	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-}
--- main/my_triangle.cpp
+++ main/my_triangle.cpp
@@ -7,133 +7,140 @@
 #include "file_utility.h"
 #include "scene.h"
 
-
 namespace gpr5300
 {
-    class MyTriangle final : public Scene
-    {
-    public:
-        void Begin() override;
-        void End() override;
-        void Update(float dt) override;
+	class MyTriangle : public Scene
+	{
+	public:
+		void Begin() override;
+		void End() override;
+		void Update(float dt) override;
 
-        float vertices[9] = {
-            0.0f, 1.0f, 0.0f,
-            -0.5f, 0.0f, 0.0f,
-            0.5f, 0.0f, 0.0f,
-        };
+		float vertices[18] =
+		{
+			0.0f, 1.0f, 0.0f,
+			-0.5f, 0.0f, 0.0f,
+			0.5f, 0.0f, 0.0f,
+			-0.5f, 0.0f, 0.0f,
+			0.0f, -1.0f, 0.0f,
+			0.5f, 0.0f, 0.0f
+		};
 
-        float colors[9] = {
-            1.0f, 1.0f, 0.0f,
-            1.0f, 0.5f, 0.0f,
-            0.0f, 0.5f, 1.0f,
-        };
+		float color[18] =
+		{
+			1.0f, 1.0f, 1.0f,
+			1.0f, 0.5f, 1.0f,
+			1.0f, 0.5f, 1.0f,
+			1.0f, 0.5f, 0.0f,
+			1.0f, 1.0f, 0.0f,
+			1.0f, 0.5f, 0.0f
+		};
 
-    private:
-        GLuint vertexShader_ = 0;
-        GLuint fragmentShader_ = 0;
-        GLuint program_ = 0;
-        GLuint vao_ = 0;
-        GLuint vbo_[2] = {};
+	private:
+		GLuint vertexShader_ = 0;
+		GLuint fragmentShader_ = 0;
+		GLuint program_ = 0;
+		GLuint vao_ = 0;
+		GLuint vbo_[2] = {};
+		GLuint ebo_ = 0;
+		float t = 0.0f;
+	};
 
-        float t = 0.0f;
+	void MyTriangle::Begin()
+	{
 
-    };
+		//VBO
+		glGenBuffers(1, &vbo_[0]);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo_[0]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glGenBuffers(1, &vbo_[1]);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo_[1]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, ebo_);
 
-    void MyTriangle::Begin()
-    {
 
-        //VBO
-        glGenBuffers(1, &vbo_[0]);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo_[0]);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        glGenBuffers(1, &vbo_[1]);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo_[1]);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+		//VAO
+		glGenVertexArrays(1, &vao_);
+		glBindVertexArray(vao_);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo_[0]);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo_[1]);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
 
-        //VAO
-        glGenVertexArrays(1, &vao_);
-        glBindVertexArray(vao_);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo_[0]);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo_[1]);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(1);
 
-       
-        //Load shaders
-        const auto vertexContent = LoadFile("data/shaders/my_triangle/triangle.vert");
-        const auto* ptr = vertexContent.data();
-        vertexShader_ = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader_, 1, &ptr, nullptr);
-        glCompileShader(vertexShader_);
-        //Check success status of shader compilation
-        GLint success;
-        glGetShaderiv(vertexShader_, GL_COMPILE_STATUS, &success);
-        if (!success)
-        {
-            std::cerr << "Error while loading vertex shader\n";
-        }
-        const auto fragmentContent = LoadFile("data/shaders/my_triangle/triangle.frag");
-        ptr = fragmentContent.data();
-        fragmentShader_ = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragmentShader_, 1, &ptr, nullptr);
-        glCompileShader(fragmentShader_);
-        //Check success status of shader compilation
+		//Load shaders
+		const auto vertexContent = LoadFile("data/shaders/my_triangle/triangle.vert");
+		const auto* ptr = vertexContent.data();
+		vertexShader_ = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader_, 1, &ptr, nullptr);
+		glCompileShader(vertexShader_);
+		//Check success status of shader compilation
+		GLint success;
+		glGetShaderiv(vertexShader_, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			std::cerr << "Error while loading vertex shader\n";
+		}
+		const auto fragmentContent = LoadFile("data/shaders/my_triangle/triangle.frag");
+		ptr = fragmentContent.data();
+		fragmentShader_ = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader_, 1, &ptr, nullptr);
+		glCompileShader(fragmentShader_);
+		//Check success status of shader compilation
 
-        glGetShaderiv(fragmentShader_, GL_COMPILE_STATUS, &success);
-        if (!success)
-        {
-            std::cerr << "Error while loading fragment shader\n";
-        }
-        //Load program/pipeline
-        program_ = glCreateProgram();
-        glAttachShader(program_, vertexShader_);
-        glAttachShader(program_, fragmentShader_);
-        glLinkProgram(program_);
-        //Check if shader program was linked correctly
+		glGetShaderiv(fragmentShader_, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			std::cerr << "Error while loading fragment shader\n";
+		}
+		//Load program/pipeline
+		program_ = glCreateProgram();
+		glAttachShader(program_, vertexShader_);
+		glAttachShader(program_, fragmentShader_);
+		glLinkProgram(program_);
+		//Check if shader program was linked correctly
 
-        glGetProgramiv(program_, GL_LINK_STATUS, &success);
-        if (!success)
-        {
-            std::cerr << "Error while linking shader program\n";
-        }
+		glGetProgramiv(program_, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			std::cerr << "Error while linking shader program\n";
+		}
 
-        //Empty vao
-        glCreateVertexArrays(1, &vao_);
-    }
+	}
 
-    void MyTriangle::End()
-    {
-        //Unload program/pipeline
-        glDeleteProgram(program_);
+	void MyTriangle::End()
+	{
+		//Unload program/pipeline
+		glDeleteProgram(program_);
 
-        glDeleteShader(vertexShader_);
-        glDeleteShader(fragmentShader_);
+		glDeleteShader(vertexShader_);
+		glDeleteShader(fragmentShader_);
 
-        glDeleteVertexArrays(1, &vao_);
+		glDeleteVertexArrays(1, &vao_);
+	}
 
-    }
+	void MyTriangle::Update(float dt)
+	{
+		t += dt;
 
-    void MyTriangle::Update(float dt)
-    {
-        t += dt;
+		//Draw program
+		glUseProgram(program_);
+		const float colorValue = (std::cos(t + 4.0f));
+		glUniform1f(glGetUniformLocation(program_, "colorCoeff"), colorValue);
+		glBindVertexArray(vao_);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        //Draw program
-        glUseProgram(program_);
-        const float colorValue = (std::cos(t + 4.0f));
-        glUniform1f(glGetUniformLocation(program_, "colorCoeff"), colorValue);
-        glBindVertexArray(vao_);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-    }
+	}
+
+
 }
+
 int main(int argc, char** argv)
 {
-    gpr5300::MyTriangle scene;
-    gpr5300::Engine engine(&scene);
-    engine.Run();
-
-    return EXIT_SUCCESS;
+	gpr5300::MyTriangle scene;
+	gpr5300::Engine engine(&scene);
+	engine.Run();
+	return EXIT_SUCCESS;
 }
-
